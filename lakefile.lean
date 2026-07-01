d2e63df3c267ac43e110ec4aae987aebcf2bcd30
DAVID J FOX <davidjfox998@gmail.com>
2026-07-01 09:35:01 -0700

    Phase 81: add NSPhase81ESSRoute root

--- lakefile.lean
+++ lakefile.lean
@@ -83,4 +83,5 @@ lean_lib Towers where
              `Towers.NS.NSPhase78GNSInterpClose,
              `Towers.NS.NSPhase79D1M5Closed,
              `Towers.NS.NSPhase80M6Routes,
+             `Towers.NS.NSPhase81ESSRoute,
              `Towers.NS.NSClayCertificateV2]
